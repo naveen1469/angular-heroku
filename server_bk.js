0af345e45be93ccd1e0a5e947765e5635309fe49
1-Naveen <naveenkumar.s@astrazeneca.com>
2019-03-08 10:26:19 +0530

    initial commit

--- server_bk.js
+++ server_bk.js
@@ -2,18 +2,11 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
+
 const app = express();
-const proxy = require('express-http-proxy');
-
-
-app.use(cors());
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/product-viewer'));
-
-// Set our api routes proxy to point to spring boot server
-app.use('/server', proxy('http://localhost:9090'));
 
 app.get('/*', function(req,res) {
     
